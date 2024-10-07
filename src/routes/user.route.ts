62a8b40c6635c4aabf4412d4375c552b25491c9e
Clément Guilbaud <115244309+DonThatch@users.noreply.github.com>
2024-10-07 15:57:18 +0200

    feat: verify authentication

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,11 +1,12 @@
 import express from "express";
 
 import {createUser, getAllUsers, getUserById, login} from "../controllers/user.controller.ts";
+import {authenticateJWT} from "../middlewares/auth.middleware.ts";
 
 const userRouter = express.Router();
 
 userRouter.get("/", getAllUsers);
-userRouter.get("/:id", getUserById);
+userRouter.get("/:id", authenticateJWT, getUserById);
 
 userRouter.post("/register", createUser);
 userRouter.post("/login", login);
