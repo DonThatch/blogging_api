151e923d7e5483fe2f3d6421efcd213c0a8aaa2f
LoicVerdier <loic.verdier@supinfo.com>
2024-10-07 15:57:53 +0200

    feat: route & item for like

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -2,17 +2,23 @@ import { Post } from "../models/post.model.ts";
 import { Request, Response } from "express";
 import { CustomError } from "../config/env.config.ts";
 import {commentSchema} from "../models/comment.model.ts";
+import {Like} from "../models/like.model.ts";
 
-export const createPost = async (req: Request, res: Response): Promise<void> => {
+export const createPost = async (req: Request, res: Response) => {
     const post = new Post({
         title: req.body.title,
         content: req.body.content,
         author: req.body.author,
         tags: req.body.tags,
     });
-    console.log(post);
+    const like = new Like({
+        postId: post._id,
+        likeNumber: 0
+    })
+
     try {
         await post.save();
+        await like.save();
         res.status(200).send(`Post ${post.title} created successfully`);
     } catch (e) {
         const error = e as CustomError;
@@ -46,6 +52,20 @@ export const modifyPost = async (req: Request, res: Response): Promise<void> => 
     }
     res.status(200).send(`Post modified successfully`);
 };
+
+export const deletePost = async (req: Request, res: Response): Promise<void> => {
+    const post = await Post.findByIdAndDelete(req.params.id);
+    try {
+        if (!post) {
+            throw { status: 404, message: "Post not found" };
+        }
+    } catch (e) {
+        const error = e as CustomError;
+        res.status(error.status || 500).send(error.message);
+    }
+    res.status(200).send(`Post deleted successfully`);
+}
+
 export const getCommentsByPostId = async (req: Request, res: Response): Promise<void> => {
     console.log("post/:id/comments")
     const comments = await commentSchema.find({postId: req.params.id});
@@ -57,5 +77,17 @@ export const getCommentsByPostId = async (req: Request, res: Response): Promise<
     } catch (e) {
         const error = e as CustomError;
         res.status(error.status || 500).send(error.message);
+    }
+}
+export const getLikeFromPost = async (req: Request, res: Response) => {
+    try {
+        const like = await Like.find({postId: req.params.postId});
+        if (like) {
+            res.json(like);
+        } else {
+            res.status(404).json({ error: "Like not found" });
+        }
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
     }
 }
--- src/models/post.model.ts
+++ src/models/post.model.ts
@@ -1,11 +1,25 @@
 import { Schema, model } from "mongoose";
+import {User} from "./user.model.ts";
 
-const postSchema = new Schema({
-    title: String,
-    content: String,
-    author: String,
-    tags: [String],
-    createDate: { type: Date, default: Date.now },
+const postSchema : Schema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: User,
+        required: true
+    },
+    tags: {
+        type:[String],
+        required: true
+    },
+    createDate: { type: Date, default: Date.now }
 });
 
 export const Post = model("Post", postSchema);
