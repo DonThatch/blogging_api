63f4d44640d33f95de3bb038d978032d1ba96555
Clément Guilbaud <115244309+DonThatch@users.noreply.github.com>
2024-10-07 14:58:47 +0200

    feat: add JWT authentication

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,12 +1,13 @@
 import express from "express";
 
-import { createUser, getAllUsers, getUserById } from "../controllers/user.controller.ts";
+import {createUser, getAllUsers, getUserById, login} from "../controllers/user.controller.ts";
 
 const userRouter = express.Router();
 
 userRouter.get("/", getAllUsers);
 userRouter.get("/:id", getUserById);
 
-userRouter.post("/", createUser);
+userRouter.post("/register", createUser);
+userRouter.post("/login", login);
 
 export default userRouter;
