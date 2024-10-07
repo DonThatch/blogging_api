5a628a8b718d78b1cddc035fdb6a4dbef7e7412d
Clément Guilbaud <115244309+DonThatch@users.noreply.github.com>
2024-10-07 16:57:47 +0200

    fix: update post and user routes

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import { createPost, getPostsById, modifyPost,getCommentsByPostId, deletePost, getLikeFromPost } from '../controllers/post.controller.ts';
 
+import { isAuthenticated } from "../middlewares/isAuth.middleware.ts";
+
 const postRouter = express.Router();
 
-postRouter.post('/', createPost);
-postRouter.get('/:id', getPostsById);
-postRouter.get('/:id/comments', getCommentsByPostId);
-postRouter.put('/:id', modifyPost);
-postRouter.delete('/:id', deletePost);
-postRouter.get('/:id/like', getLikeFromPost);
+postRouter.post('/', isAuthenticated, createPost);
+postRouter.get('/:id', isAuthenticated, getPostsById);
+postRouter.get('/:id/comments', isAuthenticated, getCommentsByPostId);
+postRouter.put('/:id', isAuthenticated, modifyPost);
+postRouter.delete('/:id', isAuthenticated, deletePost);
+postRouter.get('/:id/like', isAuthenticated, getLikeFromPost);
 
 export default postRouter;
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,14 +1,17 @@
 import express from "express";
 
-import {createUser, getAllUsers, getUserById, login} from "../controllers/user.controller.ts";
-import {authenticateJWT} from "../middlewares/auth.middleware.ts";
+import {createUser, deleteUser, getAllUsers, getUserById, login, updateUser} from "../controllers/user.controller.ts";
+import { isAuthenticated } from "../middlewares/isAuth.middleware.ts";
 
 const userRouter = express.Router();
 
 userRouter.get("/", getAllUsers);
-userRouter.get("/:id", authenticateJWT, getUserById);
+userRouter.get("/:id", isAuthenticated, getUserById);
 
 userRouter.post("/register", createUser);
 userRouter.post("/login", login);
+
+userRouter.put("/:id", isAuthenticated, updateUser);
+userRouter.delete("/:id", isAuthenticated, deleteUser);
 
 export default userRouter;
