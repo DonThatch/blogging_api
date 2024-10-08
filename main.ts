b7e17496c7d33164d3740e77dc6707d71e4403a2
Clément Guilbaud <115244309+DonThatch@users.noreply.github.com>
2024-10-08 13:11:17 +0200

    feat: add swagger
    - add dependencies (swagger-ui-express & swagger-jsdoc)
    - add swagger.yaml

--- main.ts
+++ main.ts
@@ -4,11 +4,31 @@ import "./src/config/db.config.ts";
 import { loggerMiddleware } from "./src/middlewares/logger.middleware.ts";
 import {PORT} from "./src/config/env.config.ts";
 import "./src/config/db.config.ts";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 const app = express();
 
+const swaggerSpec = swaggerJsDoc({
+  definition:{
+    openapi: "3.0.0",
+    info: {
+      title: "Blogify API",
+      version: "0.0.1",
+      description: "A simple Express Library API"
+    },
+    servers: [
+      {
+        url: "http://localhost:3000"
+      }
+    ]
+  },
+  apis: ["swagger.yaml"]
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(loggerMiddleware);
 
